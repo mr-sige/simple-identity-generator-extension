812c688e617ac8148ecd29dde5e4f2512af6db9f
sige <sige@sige.hu>
2017-11-25 17:06:51 +0100

    cleanup

---
+++ src/Content.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Content extends React.Compnent {
+    render() {
+        return(
+            <div>sige</div>
+        )
+    }
+}
+
+export default Content;
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import ReactDOM from 'react-dom';
+import Content from 'Content';
+
+const content = document.getElementById('content');
+ReactDOM.render(<div>sdf</div>, content);
